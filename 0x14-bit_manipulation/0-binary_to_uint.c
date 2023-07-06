21a2f6d8e80013a9d9db291aa3d01b94240c8118
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-07-06 19:43:06 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,16 +6,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int ui;
+	int j;
+	unsigned int ui = 0;
 if (!b)
-return (0);
-ui = 0;
-for (i = 0; b[i]; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-ui = 2 * ui + (b[i] - '0');
-}
-return (ui);
+	return (0);
+for (j = 0; b[j]; j++)
+	{
+	if (b[j] < '0' || b[j] > '1')
+	return (0);
+	ui = 2 * ui + (b[j] - '0');
+	}
+	return (ui);
 }
