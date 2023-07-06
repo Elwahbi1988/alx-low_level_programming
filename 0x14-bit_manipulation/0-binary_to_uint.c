cb4507fe149b7f6b79d7cd88a1ec2697f428674f
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-07-06 17:51:58 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,22 +3,31 @@
 /**
 *binary_to_uint - Converts a binary number to an unsigned int
 *@b: String containing the binary number
-*Return:Converted number
+*Return:Unsigned int.
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int dec_val = 0;
+int len, base_two;
+unsigned int ui = 0;
 
 if (!b)
 return (0);
 
-for (i = 0; b[i]; i++)
+ui = 0;
+
+for (len = 0; b[len] != '\0'; len++)
+	;
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
-if (b[i] < '0' || b[i] > '1')
+if (b[len] != '0' && b[len] != '1')
+{
 return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
 }
-return (dec_val);
+if (b[len] & 1)
+{
+ui += base_two;
+}
+}
+return (ui);
 }
