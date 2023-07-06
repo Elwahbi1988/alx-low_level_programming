e469267abd853e3900587a2f1e96c9469ca417af
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-07-06 15:32:49 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary: Print binary equal of a decimal number
+ * @n: Number print binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	int count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+{
+	current = n >> i;
+
+	if (current & 1)
+	{
+		_putchar('1');
+		count++;
+	}
+	else if (count)
+		_putchar('0');
+}
+if (!count)
+	_putchar('0');
+}
