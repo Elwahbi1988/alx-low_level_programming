4d9f61ebbc733f319041b8b2527153707e489594
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-07-06 16:48:11 +0000

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *set_bit:Sets bit give index 1
+ *@n: Pointer number to change
+ *@index: Index of the bit to set 1
+ *Return:1 (success), -1 failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+*n = ((1UL << index) | *n);
+return (1);
+}
