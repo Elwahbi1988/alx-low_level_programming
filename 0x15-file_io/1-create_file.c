91ca865e0d6e2907d9c96f89cd9d6918776dd100
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-07-10 17:22:49 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,31 @@
-#include 
+#include "main.h"
+
+/**
+ * create_file - File to created
+ * @filename: Pointer the file to create
+ * @text_content: Pointer the string write to file
+ * Return:--1 the function fails,- 1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int w;
+	int fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
+
